45ce84cbd29b7419f0a4f9551052d3c5e461e14e
KevinU <115033945+kevins2417@users.noreply.github.com>
2024-07-06 13:35:48 -0500

    Update MainFragment.kt

--- app0.2/src/main/java/com/hause/pianoapp/MainFragment.kt
+++ app0.2/src/main/java/com/hause/pianoapp/MainFragment.kt
@@ -10,22 +10,30 @@ import androidx.fragment.app.Fragment
 import com.tayyar.tiletap.databinding.FragmentMainBinding
 import com.tayyar.tiletap.game.GameActivity
 
-/** Homepage of the app */
+/** 
+ * MainFragment representa la pantalla principal de la aplicación. 
+ * Muestra opciones para configurar el juego y permite iniciar la actividad del juego.
+ */
 class MainFragment : Fragment() {
 
+    /**
+     * onCreateView es llamado para inflar el layout del fragmento y configurar los listeners de los botones.
+     * Aquí se manejan las interacciones del usuario para iniciar el juego con las opciones seleccionadas.
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding: FragmentMainBinding = FragmentMainBinding.inflate(inflater, container, false)
 
-        // Add on click listener to the button to start the game
+        // Configura el listener del botón para iniciar el juego
         binding.startButton.setOnClickListener {
             val speed = binding.speedInput.text.toString()
             val music = binding.musicBox.isChecked
             val vibration = binding.vibrationBox.isChecked
             val speedIncrease = binding.speedIncreaseBox.isChecked
 
+            // Verifica si la velocidad es válida antes de iniciar el juego
             if (speed == "" || speed == "0") {
                 Toast.makeText(context, "You have to select a speed", Toast.LENGTH_SHORT).show()
             } else {
